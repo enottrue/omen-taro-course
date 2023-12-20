9c414cd2aaffc00bcdf1b7163bf91155dfd6d518
Ivan D <bad_vano@list.ru>
2023-12-21 02:32:45 +0300

    Login useHook and Button implementation Fixes #12

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -95,3 +95,20 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        email
+        name
+        city
+        phone
+      }
+      message
+      error
+    }
+  }
+`;
---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,34 @@
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '@/graphql/queries';
+
+export const useLoginUser = () => {
+  const [loginUser, { data, loading, error }] = useMutation(LOGIN_USER);
+
+  const login = async (email: String, password: String) => {
+    try {
+      const response = await loginUser({ variables: { email, password } });
+      console.log('LoginUser', response);
+      return {
+        user: response.data?.loginUser?.user, //?.loginUser.user,
+        error: response.data?.loginUser?.error,
+        message: response.data?.loginUser?.message,
+        token: response.data?.loginUser?.token,
+      };
+      return response.data.loginUser;
+    } catch (err) {
+      throw err;
+      //   return {
+      //     message: (err as Error).message,
+      //     error: true,
+      //   };
+    }
+  };
+
+  return {
+    login,
+    loading,
+    error,
+  };
+};
+
+export default useLoginUser;
