8c00de7be4ac9c7df2273be8fcd4d57c173836ce
enottrue <enottrue@gmail.com>
2025-07-11 09:55:58 +0300

    fix font size

--- src/components/course_book/courseBook.tsx
+++ src/components/course_book/courseBook.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import book from '@/images/tu@2x.png';
 import Image from 'next/image';
 import Button from '../button/Button';
@@ -8,7 +8,8 @@ import { MainContext } from '@/contexts/MainContext';
 import styles from '@/components/component1/component1.module.css';
 import unsplashImage from '../../images/unsplashutbx9x3y8ly-2@2x.png';
 import image3 from '../../images/image-3@2x.png';
-import AuthNav from '../header/AuthNav';
+
+import BurgerMenu from '../component1/BurgerMenu';
 
 interface CourseBookHeroProps {
   token?: string | null;
@@ -18,6 +19,29 @@ interface CourseBookHeroProps {
 const CourseBookHero = ({ token, userId }: CourseBookHeroProps) => {
   const router = useRouter();
   const cc = useContext(MainContext);
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+  const burgerRef = useRef<HTMLDivElement>(null);
+
+  const handleBurgerClick = () => {
+    setIsBurgerOpen(!isBurgerOpen);
+  };
+
+  // Close burger menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (burgerRef.current && !burgerRef.current.contains(event.target as Node)) {
+        setIsBurgerOpen(false);
+      }
+    };
+
+    if (isBurgerOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isBurgerOpen]);
 
   return (
     <div className="root">
@@ -71,64 +95,20 @@ const CourseBookHero = ({ token, userId }: CourseBookHeroProps) => {
                   <div className={styles.div}>Sign In</div>
                 </div>
               ) : (
-                <Button
-                  title="Меню"
-                  className="button_little js-header__menu-toggle"
-                  onClick={() => {
-                    cc?.setMenuOpen(!cc?.menuOpen);
-                  }}
+                <div 
+                  ref={burgerRef}
+                  className={styles.burgerMenu}
+                  onClick={handleBurgerClick}
+                  style={{ cursor: 'pointer', marginLeft: '10px', position: 'relative', minWidth: 'fit-content' }}
                 >
-                  <span className="button__icon">
-                    <span className="header__icon">
-                      <span className="header__icon-open">
-                        <svg width="800" height="800" viewBox="0 0 20 20" fill="#FFF">
-                          <g
-                            id="Page-1"
-                            fillRule="evenodd"
-                            stroke="none"
-                            strokeWidth="1"
-                          >
-                            <g
-                              id="Dribbble-Light-Preview"
-                              transform="translate(-140 -2159)"
-                            >
-                              <g id="icons" transform="translate(56 160)">
-                                <path
-                                  id="profile_round-[#1342]"
-                                  d="M100.56 2017H87.44c-.7 0-1.23-.7-.96-1.34 1.23-2.96 4.14-4.66 7.52-4.66s6.29 1.7 7.52 4.66c.27.64-.25 1.34-.96 1.34m-10.64-12c0-2.2 1.83-4 4.08-4s4.08 1.8 4.08 4-1.83 4-4.08 4a4.05 4.05 0 0 1-4.08-4m14.04 11.64a9.52 9.52 0 0 0-6.12-6.97 5.93 5.93 0 0 0 2.21-5.6 6.1 6.1 0 0 0-5.32-5.03 6.08 6.08 0 0 0-6.85 5.96c0 1.89.89 3.57 2.28 4.67a9.52 9.52 0 0 0-6.12 6.97c-.27 1.22.74 2.36 2.01 2.36h15.9c1.27 0 2.28-1.14 2-2.36"
-                                />
-                              </g>
-                            </g>
-                          </g>
-                        </svg>
-                      </span>
-                      <span className="header__icon-close">
-                        <svg
-                          width="22"
-                          height="22"
-                          viewBox="0 0 22 22"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M16.2791 5.61243L5.50409 16.3874"
-                            stroke="#fff"
-                            strokeWidth="2"
-                          />
-                          <path
-                            d="M5.50427 5.61243L16.2792 16.3874"
-                            stroke="#fff"
-                            strokeWidth="2"
-                          />
-                        </svg>
-                      </span>
-                    </span>
-                  </span>
-                </Button>
+                  <div className={styles.burgerLine}></div>
+                  <div className={styles.burgerLine}></div>
+                  <div className={styles.burgerLine}></div>
+                  <BurgerMenu isOpen={isBurgerOpen} onClose={handleBurgerClick} />
+                </div>
               )}
             </div>
           </header>
-          {token && <AuthNav />}
         <div className="cource-book__img">
           <Image src={book} alt="Электронная методичка" priority />
         </div>
