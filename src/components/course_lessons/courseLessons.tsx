623506cb5dd786320b95e0bc1e04723d23148508
enottrue <enottrue@gmail.com>
2025-07-31 13:28:53 +0300

    fix image quality

--- src/components/course_lessons/courseLessons.tsx
+++ src/components/course_lessons/courseLessons.tsx
@@ -17,8 +17,8 @@ export const CourseLessons = ({ lessons }: { lessons?: any[] }) => {
   const user = cc?.user;
   const isUserAuthorized = userId && user && user.id;
   
-  console.log('CourseLessons received lessons:', lessons?.length || 0);
-  console.log('User authorized:', isUserAuthorized);
+  // console.log('CourseLessons received lessons:', lessons?.length || 0);
+  // console.log('User authorized:', isUserAuthorized);
 
   return (
     <div className="course-lessons-outer-wrapper" style={{ position: 'relative', width: '100%', maxWidth: 370, margin: '0 auto' }}>
@@ -31,6 +31,10 @@ export const CourseLessons = ({ lessons }: { lessons?: any[] }) => {
           height={321} 
           className="course-lessons__laptop-img"
           style={{ pointerEvents: 'none' }}
+          quality={95}
+          priority={true}
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
         />
       </div>
      
